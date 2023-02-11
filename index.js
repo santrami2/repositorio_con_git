680d41a28f6b8ebf321771e0f233e9ed4d9ff695
Santiago Ramirez <santiagoramirezagudelo@gmail.com>
2023-02-11 11:01:45 +0100

    agregando funciones

---
+++ index.js
@@ -0,0 +1,18 @@
+const user1 = {
+    nombre: 'Juan',
+    apellido: 'Perez',
+    edad: 20,
+    peso: 100
+}
+
+console.log(user1.direccion);
+
+console.log("hola mundo")
+
+alert("hola mundo yo estoy probando javascript con html")
+
+function init() {
+    //document.getElementById("probandojavascript").style.display = "none";
+}
+
+init();
